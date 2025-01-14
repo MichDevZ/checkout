9161d9946310b9bc106f77b4373aea47a256e191
Michdev <Michael.devtloz@mail.com>
2025-01-13 22:04:59 -0300

    Fixed pickup3

--- src/app/admin/page.jsx
+++ src/app/admin/page.jsx
@@ -1,57 +1,57 @@
-'use client'
+// 'use client'
 
-import axios from 'axios';
-import { useState } from 'react';
+// import axios from 'axios';
+// import { useState } from 'react';
 
 
-const adminPage = () => {
+// const adminPage = () => {
 
-    const [file, setFile] = useState(null);
+//     const [file, setFile] = useState(null);
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    if (!file) return;
+//   const handleSubmit = async (e) => {
+//     e.preventDefault();
+//     if (!file) return;
 
-    const formData = new FormData();
-    formData.append('file', file);
+//     const formData = new FormData();
+//     formData.append('file', file);
 
-    const response = await fetch('/api/handle-file', {
-      method: 'POST',
-      body: formData,
-    });
+//     const response = await fetch('/api/handle-file', {
+//       method: 'POST',
+//       body: formData,
+//     });
     
-    const data = await response.json();
+//     const data = await response.json();
 
-    if (data) {
-        const {response} = await axios.post('/api/send-product', {
-          data: data[0]
-        })
-    }
+//     if (data) {
+//         const {response} = await axios.post('/api/send-product', {
+//           data: data[0]
+//         })
+//     }
 
-  };
+//   };
 
 
 
-  return (
-    <div className='flex justify-center'>
-      <div className='bg-gray-200 w-[500px] px-10 py-5 items-center flex flex-col'>
-            <h1 className='font-bold mb-5'>Sube tu excel con tus productos!</h1>
+//   return (
+//     <div className='flex justify-center'>
+//       <div className='bg-gray-200 w-[500px] px-10 py-5 items-center flex flex-col'>
+//             <h1 className='font-bold mb-5'>Sube tu excel con tus productos!</h1>
 
-            <form onSubmit={(e) => handleSubmit(e)} >
+//             <form onSubmit={(e) => handleSubmit(e)} >
 
-            <div className='flex flex-col'>
+//             <div className='flex flex-col'>
 
-            <input className='mb-5' type="file" accept=".xlsx, .xls" onChange={(e) => setFile(e.target.files[0])} />
-            <button className='text-white px-2 py-1 rounded-md bg-blue-500' type="submit"
-            >Subir Archivo</button>
+//             <input className='mb-5' type="file" accept=".xlsx, .xls" onChange={(e) => setFile(e.target.files[0])} />
+//             <button className='text-white px-2 py-1 rounded-md bg-blue-500' type="submit"
+//             >Subir Archivo</button>
 
-            </div>
+//             </div>
 
-            </form>
+//             </form>
 
-      </div>
-    </div>
-  )
-}
+//       </div>
+//     </div>
+//   )
+// }
 
-export default adminPage
+// export default adminPage
